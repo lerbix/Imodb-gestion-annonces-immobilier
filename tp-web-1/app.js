9ed5af219ffb1ee917c224819779b020f6585973
El Arbi Rikab <elarbi.rikab@etu.univ-lehavre.fr>
2023-09-25 16:08:29 +0200

    switch to ejs

--- tp-web-1/app.js
+++ tp-web-1/app.js
@@ -21,7 +21,7 @@ const connectDB = require('./config/connectionDb');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -72,7 +72,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {title : "Erreur"} );
 });
 
 module.exports = app;
