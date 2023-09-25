a9edb7540653240ad2bd3210828c791449c5d748
ilyass <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2023-09-25 13:54:54 +0200

    check if authenticated using the middleware

--- tp-web-1/routes/index.js
+++ tp-web-1/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var {checkIfAuthenticated}= require('../controler/authControler');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', checkIfAuthenticated, function(req, res, next) {
+  res.render('index', { title: req.user.username, admin: req.user.isAdmin });
 });
+
 
 module.exports = router;
