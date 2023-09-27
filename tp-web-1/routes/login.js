0e3fa2c61b34bb023622693e859ecdd98bc053ba
ilyass <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2023-09-27 17:29:59 +0200

    login front page

--- tp-web-1/routes/login.js
+++ tp-web-1/routes/login.js
@@ -6,7 +6,11 @@ var {redirectIfAuthenticated} = require('../controler/authControler')
 
 
 router.post('/', passport.authenticate('local'), function(req, res) {
-
+    // Check for authentication errors
+  if (!req.user) {
+    // Handle non-existent user or incorrect password
+    return res.redirect('/login?error=Invalid%20username%20or%20password');
+  }
     req.session.save((err) => {
         if (err) return res.render('/login');
         req.session.passport.idUtilisateur = req.user._id;
@@ -17,9 +21,11 @@ router.post('/', passport.authenticate('local'), function(req, res) {
 
 });
 
-router.get('/', redirectIfAuthenticated ,function(req, res) {
-    res.render('login', {user: req.user});
-});
+router.get('/', redirectIfAuthenticated, function (req, res) {
+    const error = req.query.error;
+    res.render('login', { error, user: req.user });
+  });
+  
 
 router.get('/logout', function(req, res) {
     req.logout(function(err) {
