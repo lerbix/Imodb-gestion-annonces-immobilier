bcbec1583834dbd5ed72e569e7c5ecddb40ae228
El Arbi Rikab <elarbi.rikab@etu.univ-lehavre.fr>
2023-09-29 17:45:10 +0200

    add annonce details back end

--- tp-web-1/controler/annoncesController.js
+++ tp-web-1/controler/annoncesController.js
@@ -5,7 +5,6 @@ const getAllAnnonces = async (req, res, next) => {
     try {
         const annonces = await Annonce.find({}).exec(); // Utilisez .exec() pour exécuter la requête
     
-        console.log('Annonces Récupérées : ', annonces)
         res.render('annonces', { title: 'Annonces', annonces: annonces, isAdmin: req.user ? req.user.isAdmin : undefined  });
       } catch (err) {
         console.error('Erreur lors de la récupération des annonces :', err);
@@ -13,6 +12,33 @@ const getAllAnnonces = async (req, res, next) => {
       }
 };
 
+
+const getAnnonceInfo = async (req, res, next)=> {
+  const annonceId = req.params.id;
+  
+  try {
+    // Utilisez Mongoose pour rechercher l'annonce par son ID
+    const annonce = await Annonce.findById(annonceId).exec();
+
+    if (!annonce) {
+      return res.status(404).send('Aucune annonce trouvée avec cet ID');
+    }
+
+  
+    console.log(annonce)
+    // Si une annonce est trouvée, vous pouvez renvoyer les détails de l'annonce à la vue
+    res.render('annonceInformations', { 
+      title: 'Détails de l\'annonce',
+      annonce: annonce,
+      isAdmin: req.user ? req.user.isAdmin : undefined
+    });
+  } catch (err) {
+    console.error('Erreur lors de la récupération des détails de l\'annonce :', err);
+    return next(err);
+  }
+}
+
 module.exports = {
-    getAllAnnonces
+    getAllAnnonces,
+    getAnnonceInfo
 };
