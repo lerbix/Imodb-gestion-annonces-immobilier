ef1b8949d53064d5b0572f23e7bd9d61df1de718
ilyass <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2023-09-29 18:23:59 +0200

    Implement creating new annonces in the controller

--- tp-web-1/controler/annoncesController.js
+++ tp-web-1/controler/annoncesController.js
@@ -13,6 +13,41 @@ const getAllAnnonces = async (req, res, next) => {
       }
 };
 
+// Crée une nouvelle annonce
+const createAnnonce = (req, res, next) => {
+  // Valider la requête
+  if (!req.body.titre || !req.body.type || !req.body.statusPublication || !req.body.statusBien || !req.body.descriptionLongue || !req.body.prix || !req.body.dateDisponibilite) {
+      return res.status(400).send({ message: "Tous les champs doivent être remplis !" });
+  }
+
+  // Créer un objet Annonce
+  const annonce = new Annonce({
+      titre: req.body.titre,
+      type: req.body.type,
+      statusPublication: req.body.statusPublication,
+      statusBien: req.body.statusBien,
+      descriptionLongue: req.body.descriptionLongue,
+      prix: req.body.prix,
+      dateDisponibilite: req.body.dateDisponibilite,
+      photos: 'hero.jpg',
+  });
+
+  // Enregistrer l'objet dans la base de données
+  annonce.save()
+      .then(data => {
+          res.redirect('/annonces');
+      })
+      .catch(err => {
+          res.status(500).send({
+              message: "Une erreur s'est produite lors de la création de l'annonce : " + err.message
+          });
+      });
+};
+
+  
+
+
 module.exports = {
-    getAllAnnonces
+    getAllAnnonces,
+    createAnnonce
 };
