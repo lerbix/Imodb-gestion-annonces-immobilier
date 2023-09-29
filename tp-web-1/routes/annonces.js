518b55da818faa2e169ad36a8dfc9cdc3fa672b6
ilyass <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2023-09-29 18:21:51 +0200

    Implement route for creating new annonces

--- tp-web-1/routes/annonces.js
+++ tp-web-1/routes/annonces.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var {checkIfAuthenticated}= require('../controler/authControler');
-var {getAllAnnonces} =require('../controler/annoncesController')
+var {getAllAnnonces, createAnnonce} =require('../controler/annoncesController');
+const annonces = require('../models/annonces');
 /* all annonces . */
 router.get('/', getAllAnnonces);
+
+/* Crée une annonce*/
+router.get('/add', function(req, res) {
+    if (req.session.passport === undefined || req.session.passport.isAdmin === false)
+        res.redirect("/")
+    else{
+        console.log(req.session.passport.isAdmin);
+        res.render("add",{title:'Add a Listing',isAdmin: req.user ? req.user.isAdmin : undefined});
+    }
+});
+
+
+
 
 
 module.exports = router;
