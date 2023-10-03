13a3ec8d8f7de8f43889d5902b0a245651a63018
El Arbi Rikab <elarbi.rikab@etu.univ-lehavre.fr>
2023-10-03 10:52:59 +0200

    change folders for images update

--- tp-web-1/controler/multerController.js
+++ tp-web-1/controler/multerController.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // Define the destination directory for uploaded images
-    cb(null, "./public/images/");
+    cb(null, "./public/images/uploads");
   },
   filename: function (req, file, cb) {
     // Define the filename for uploaded images
